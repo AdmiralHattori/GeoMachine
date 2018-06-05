3c55ca9893aff445c173246fd6ccc84c6d142066
AdmiralHattor <admiralhattori@gmail.com>
2018-06-05 00:28:23 -0700

    Added message to output the area of triangle

--- main.cpp
+++ main.cpp
@@ -12,14 +12,17 @@ int main()
   double tri_area;
 
   // Finding the area of the triangle
+  cout << endl;
   cout << "--------------------------" << endl;
   cout << "Area of Triangle" << endl;
   cout << "--------------------------" << endl;
-  cout << endl;
   cout << "Enter the base: ";             // Prompt user for the base of the triangle
   cin >> tri_base;                        // Store the entered base into tri_base
   cout << "Enter the height: ";           // Prompt user for the height of the triangle
   cin >> tri_height;                      // Store the entered height into tri_height
   tri_area = A_tri(tri_base, tri_height); // Calls the A_tri function
+  cout << "Computed area of triangle: " << tri_area << endl;
+
+  // PROGRAM TERMINATES
   return 0;
 }
